319a67263e31aa588ddcc1dab716248ac79ca3e1
Mark S. <127447378+voidsMark@users.noreply.github.com>
2024-05-15 17:35:43 +0500

    feat: linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-empty-function': 'error',
+    'no-useless-constructor': 'error',
+    'import/no-unresolved': 'off',
+    'no-console': 'off',
+    'no-restricted-syntax': 'off',
+    'no-continue': 'off',
+    'quote-props': 'off',
+    'vue/require-v-for-key': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['error', 'never', {
+      'json': 'always',
+      'vue': 'always',
+    }],
+    'arrow-body-style': ['error', 'as-needed'],
+    'semi': ['error', 'never'],
+    'linebreak-style': 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 4,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
+}
